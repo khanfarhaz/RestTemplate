ad265c01906ccbbd928a3472143d504ad2b07c0f
khanfarhaz <92144622+khanfarhaz@users.noreply.github.com>
2022-01-17 15:42:28 +0530

    Add files via upload

---
+++ collegedataapi/src/main/java/com/example/collegedataapi/CollegedataapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.collegedataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegedataapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegedataapiApplication.class, args);
+	}
+
+}
